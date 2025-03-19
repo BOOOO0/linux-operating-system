bb8a72418e33ac132eb1c7c7c4ada937c0f1e497
BOOOO0 <sunarol@naver.com>
2025-03-19 10:43:07 +0900

    feat: bandit level 3

---
+++ solve-problems/bandit/3.sh
@@ -0,0 +1,5 @@
+# 리눅스 명령어에서 문자를 있는 그대로 문자로 사용하기 위해 백슬래시 사용하는 문제
+cat ./spaces\ in\ this\ filename 
+# 정규표현식에서도 .같은 문자를 문자로 그대로 사용하기 위해 \.로 작성하는 것과 동일하다.
+# bandit3 passwd - MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx
+ssh bandit3@bandit.labs.overthewire.org -p 2220
